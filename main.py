42cc0315b1a3ad0cc602f537e9120b9fd33e38ea
taehoy <rethnoly@gmail.com>
2025-05-05 15:09:44 +0900

    feat: 상표등록상태 필터기능 구현

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from enum import Enum
+from typing import Optional
 import json
 
 app = FastAPI()
+
+class RegisterStatus(str, Enum):
+    APPLICATION = "출원",
+    REGISTRATION = "등록",
+    INVALIDATION = "실효",
+    REJECTION = "거절"
 
 @app.get("/")
 async def root(page: int = 1, limit: int = 10):
@@ -13,12 +21,24 @@ async def root(page: int = 1, limit: int = 10):
     return data[start: end]
 
 @app.get("/search")
-async def search(q: str, page: int = 1, limit: int = 10):
+async def search(
+    q: Optional[str] = Query(None),
+    status: Optional[str] = Query(None),
+    page: int = 1,
+    limit: int = 10
+    ):
+    print(q)
     start = (page - 1) * limit
     end = start + limit
     with open("trademark_sample.json", "r") as f:
         data = json.load(f)
-    
-    filtered_data = [item for item in data if q.lower() in str(item["productName"])]
 
-    return filtered_data[start: end]
+    # 상표현황 필터링
+    if status:
+        data = [item for item in data if item["registerStatus"] == status]
+
+    # 상표명 검색 필터링
+    if q and q.rstrip():
+        data = [item for item in data if q.lower() in str(item["productName"])]
+
+    return data[start: end]
