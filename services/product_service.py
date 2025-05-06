c87b2597b8f47f2776c5bef1c4d515402b72d9c5
taehoy <rethnoly@gmail.com>
2025-05-06 16:58:05 +0900

    feat: 결측치 처리 로직 service->repository로 이동

--- services/product_service.py
+++ services/product_service.py
@@ -12,47 +12,6 @@ class ProductService:
 
         data = product_repository.load_data()
         
-        # 상품명 필터링 - 상품명과 상품명 영문명이 모두 비어있는 경우
-        for item in data:
-            if (item["productName"] == "" or not item["productName"]) and  (item["productNameEng"] == "" or not item["productNameEng"]):
-                data.remove(item)
-        
-        # 상품 주분류코드리스트가 비어있는 경우 -> 현재 없음
-        for item in data:
-            if len(item["asignProductMainCodeList"]) == 0:
-                # print(item["asignProductMainCodeList"])
-                data.remove(item)
-
-        # 출원번호(일자) 필터링 - 출원번호(일자)가 비어있는 경우 -> 현재 없음
-        for item in data:
-            if (item["applicationNumber"] == "" or not item["applicationNumber"]) and (item["applicationDate"] == "" or not item["applicationDate"]):
-                # print(item["applicationNumber"], item["applicationDate"])
-                data.remove(item)
-        
-        # 상표 등록상태가 "등록"인 경우, 등록번호, 공고번호가 비어있는 경우
-        for item in data:
-            if item["registerStatus"] == "등록" and ((item["registrationNumber"] == "" or not item["registrationNumber"]) or (item["publicationNumber"] == "" or not item["publicationNumber"])):
-                # print(item["registerStatus"], item["registrationNumber"], item["publicationNumber"])
-                data.remove(item)
-
-        # 상표 등록상태가 "실효"인 경우, 등록번호 또는 공고번호가 없는 경우
-        for item in data:
-            if item["registerStatus"] == "실효" and ((item["registrationNumber"] == "" or not item["registrationNumber"]) or (item["publicationNumber"] == "" or not item["publicationNumber"])):
-                # print(item["registerStatus"], item["registrationNumber"], item["publicationNumber"])
-                data.remove(item)
-
-        # 상표 등록상태가 "거절"인 경우, 등록번호 또는 공고번호가 있는 경우
-        for item in data: 
-            if item["registerStatus"] == "거절" and (item["registrationNumber"] or item["publicationNumber"]):
-                # print(item["registerStatus"], item.get("registrationNumber"), item.get("publicationNumber"))
-                data.remove(item)
-
-        # 상표 등록상태가 "출원"인 경우, 등록번호 또는 공고번호가 있는 경우 -> 현재 없음
-        for item in data: 
-            if item["registerStatus"] == "출원" and (item["registrationNumber"] or item["publicationNumber"]):
-                # print("결측 데이터 발견:", item["registerStatus"], item.get("registrationNumber"), item.get("publicationNumber"))
-                data.remove(item) 
-        
         return data[start: end]
     
     def get_search_trademark_data(
