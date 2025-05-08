159e42b16e845979fc23a108ab0a31e918cc447d
taehoy <rethnoly@gmail.com>
2025-05-08 15:40:51 +0900

    feat: lang, order 오타 발생시 예외처리

--- routers/product_router.py
+++ routers/product_router.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter,Query, Depends
+from fastapi import APIRouter,Query, Depends, HTTPException, status
 from enum import Enum
 from typing import Optional
 
 from services.product_service import ProductService
 from repositories.product_repository import ProductRepository
 from utils.utils import clean_nulls
+from utils.utils import validate_query
 
 ProductRouter = APIRouter()
 
@@ -26,6 +27,13 @@ async def root(order: str = "asc",
             limit: int = 10,
             service: ProductService = Depends(get_product_service)
             ):
+    
+    if order not in ["asc", "desc"]:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"유효하지 않은 'order' 파라미터 값입니다. 'asc' 또는 'desc'로 입력하시길 바랍니다. 입력한 order 값 : {order}."
+            )
+    
     data = service.get_all_trademark_data(order, page, limit)
     result = [clean_nulls(item) for item in data]
 
@@ -41,6 +49,14 @@ async def search(
     limit: int = 10,
     service: ProductService = Depends(get_product_service)
     ):
+
+    validate_query(q)
+    
+    if lang not in ["ko", "en"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"언어 설정(lang)은 'ko'(한글) 또는 'en'(영어)만 지원합니다. 현재 입력한 lang : {lang}. "
+        )
 
     data = service.get_search_trademark_data(
         q=q,
